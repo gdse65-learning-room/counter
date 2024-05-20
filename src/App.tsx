7848b09b766e6b3774c6e08692368cfd3a17b345
danujav <danujagreru@gmail.com>
2024-05-20 10:22:44 +0530

    render my self

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import User from "./User";
+
+function App() {
+  return (
+      <User />
+  );
+}
+
+export default App;
---
+++ src/User.tsx
@@ -0,0 +1,18 @@
+function User() {
+  const name = "Danuja";
+  const age = 30;
+  const bestFriend = "Sirimal";
+
+  return (
+    <>
+      <h1>User Information</h1>
+      <h2>My name is {name}</h2>
+      <h2>I'm {age} years old</h2>
+      <h2>My best friend is {bestFriend}</h2>
+    </>
+  );
+}
+
+export default User;
+
+// Fragments
